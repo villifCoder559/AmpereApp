e1c12443a5882cfe399a121a4ffa1614e2f7769a
villifCoder559 <francesco.villi@gmail.com>
2021-09-15 00:17:26 +0200

    change routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './guards/auth.guard';
 import { AutoLoginGuard } from './guards/auto-login.guard';
 import { IntroGuard } from './guards/intro.guard';
 const routes: Routes = [
@@ -10,27 +9,21 @@ const routes: Routes = [
     canLoad: [IntroGuard, AutoLoginGuard],
   },
   {
-    path: 'intro',
-    loadChildren: () => import('./pages/intro/intro.module').then(m => m.IntroPageModule)
-  },
-  {
-    path: 'tabs',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule),
-    canLoad: [AuthGuard]
-  },
-  {
-    path:'',
-    redirectTo:'/login',
-    pathMatch:'full'
-  },
-  {
     path: 'signup',
-    loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
+    loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupPageModule)
   },
   {
-    path: 'registration-devices',
-    loadChildren: () => import('./pages/registration-devices/registration-devices.module').then( m => m.RegistrationDevicesPageModule)
-  }
+    path: '',
+    loadChildren: () => import('./pages/menu/menu.module').then(m => m.MenuPageModule)
+  },
+  {
+    path: 'show-alert',
+    loadChildren: () => import('./show-alert/show-alert.module').then( m => m.ShowAlertPageModule)
+  },
+  // {
+  //   path: 'menu',
+  //   loadChildren: () => import('./pages/menu/menu.module').then(m => m.MenuPageModule)
+  // }
 ];
 @NgModule({
   imports: [
