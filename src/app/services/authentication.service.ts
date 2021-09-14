131b2d6bae29f69bd6f53223013e1cebece70335
villifCoder559 <francesco.villi@gmail.com>
2021-09-15 00:16:46 +0200

    add flag logged

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, tap, switchMap } from 'rxjs/operators';
 import { BehaviorSubject, from, Observable, Subject } from 'rxjs';
- 
+import { SharedDataService } from '../data/shared-data.service'
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
   isAuthenticated: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(null);
   token = '';
-  constructor(private http:HttpClient) {
+  constructor(private http:HttpClient,private shared_data:SharedDataService) {
     this.loadToken();
    }
 
@@ -19,6 +19,7 @@ export class AuthenticationService {
     if (token != null) {
       console.log('set token: ', token);
       this.token = token;
+      this.shared_data.setIs_logged(true);
       this.isAuthenticated.next(true);
     } else {
       this.isAuthenticated.next(false);
@@ -30,13 +31,15 @@ export class AuthenticationService {
     return this.http.post(`https://reqres.in/api/login`, credentials).pipe(
       map((data: any) => {
         this.token=data.token;
-        window.localStorage.setItem('TOKEN_KEY',this.token);;
+        window.localStorage.setItem('TOKEN_KEY',this.token);
+        this.shared_data.setIs_logged(true);
         this.isAuthenticated.next(true);})
     )
   }
 
   logout(){
     this.isAuthenticated.next(false);
+    this.shared_data.setIs_logged(false);
     window.localStorage.removeItem('TOKEN_KEY'); 
   }
 }
