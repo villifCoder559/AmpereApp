4d03fd6a5abf996312ec62e924234cc1fd507478
villifCoder559 <francesco.villi@gmail.com>
2021-09-15 00:17:37 +0200

    add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,15 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { MatSelectModule } from "@angular/material/select";
-import { MatIconModule } from "@angular/material/icon";
-import { MatInputModule } from "@angular/material/input";
 import {Contacts} from '@ionic-native/contacts'
 import { BLE } from '@ionic-native/ble/ngx'
-
+import {MaterialModule} from '../app/material-module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [MatInputModule,MatIconModule,MatSelectModule,BrowserModule, BrowserAnimationsModule,MatFormFieldModule,
-            IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
+  imports: [BrowserModule, BrowserAnimationsModule,MatFormFieldModule,
+            IonicModule.forRoot(), AppRoutingModule,HttpClientModule,MaterialModule
+            ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },Contacts,BLE],
   bootstrap: [AppComponent],
 })
