8cb0d14958d0458dceda628103ef6edbdfbbfac3
villifCoder559 <francesco.villi@gmail.com>
2021-10-14 09:41:49 +0200

    add snap4city login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -4,6 +4,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertController, LoadingController, ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { SharedDataService } from '../../data/shared-data.service'
+import * as Keycloak from 'keycloak-ionic/keycloak';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -11,15 +13,15 @@ import { SharedDataService } from '../../data/shared-data.service'
 })
 export class LoginPage implements OnInit {
   credentials: FormGroup;
-
+  public keycloak: Keycloak.KeycloakInstance;
   constructor(
     private fb: FormBuilder,
     private authService: AuthenticationService,
     private alertController: AlertController,
     private router: Router,
     private loadingController: LoadingController,
-    private sharedData:SharedDataService,
-    private toastCtrl:ToastController
+    private sharedData: SharedDataService,
+    private toastCtrl: ToastController
   ) { }
 
   ngOnInit() {
@@ -30,7 +32,7 @@ export class LoginPage implements OnInit {
   }
 
   async login() {
-      const loading = await this.loadingController.create();
+    const loading = await this.loadingController.create();
     await loading.present();
 
     this.authService.login(this.credentials.value).subscribe(
@@ -57,27 +59,53 @@ export class LoginPage implements OnInit {
   get password() {
     return this.credentials.get('password');
   }
-  go_to_signup(){
+  go_to_signup() {
     this.router.navigateByUrl('/signup');
   }
-  async go_to_forgot_psw(){
-    if( this.email.hasError('email'))
+  async go_to_forgot_psw() {
+    if (this.email.hasError('email'))
       this.show_toast('Insert valid email in the field');
-    else if(this.email.hasError('required'))
-        this.show_toast('Email field is required');
-        else{
-          const alert = await this.alertController.create({
-            header: 'To reset your password follow the instructions sent to your email',
-            buttons: ['OK'],
-          });
-          await alert.present();
-        }
+    else if (this.email.hasError('required'))
+      this.show_toast('Email field is required');
+    else {
+      const alert = await this.alertController.create({
+        header: 'To reset your password follow the instructions sent to your email',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    }
   }
-  async show_toast(txt){
+  async show_toast(txt) {
     let toast = this.toastCtrl.create({
       header: txt,
       duration: 2500
     })
-    ;(await toast).present();
+      ; (await toast).present();
+  }
+
+  autentication() {
+    if (this.keycloak == null) {
+      console.log('keycloak null')
+      this.keycloak = Keycloak({
+        clientId: 'js-snap4city-mobile-app',
+        realm: 'master',
+        url: "https://www.snap4city.org/auth/"
+      });
+    }
+    this.keycloak.init({
+      onLoad: 'login-required',
+      adapter:'cordova'
+    }).then((autentication) => {
+      console.log(autentication)
+      if(autentication)
+        alert("Success Auth")
+      else
+        this.keycloak.login();
+    }, (err) => alert(err));
+
+    // Test if it works, when coming back from this.keycloak.login();
+    this.keycloak.onAuthSuccess = () => {
+      console.log('authenticated!');
+    };
   }
 }
