ab69c9df849d8f26645135a0184eb12c99b67a16
villifCoder559 <francesco.villi@gmail.com>
2021-10-27 00:02:13 +0200

    update path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AutoLoginGuard } from './guards/auto-login.guard';
 import { IntroGuard } from './guards/intro.guard';
+import { BLE } from '@ionic-native/ble/ngx'
 const routes: Routes = [
   {
     path: 'login',
@@ -18,17 +19,19 @@ const routes: Routes = [
   },
   {
     path: 'show-alert',
-    loadChildren: () => import('./show-alert/show-alert.module').then( m => m.ShowAlertPageModule)
+    loadChildren: () => import('./show-alert/show-alert.module').then(m => m.ShowAlertPageModule)
   },
   {
     path: '',
-    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule),
+    canLoad: [IntroGuard, AutoLoginGuard],
   }
 ];
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [BLE]
 })
 export class AppRoutingModule { }
