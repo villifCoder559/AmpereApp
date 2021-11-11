15f455e789daa5c864643d08ab60cffb464cb256
villifCoder559 <francesco.villi@gmail.com>
2021-11-11 17:26:10 +0100

    add list of QRs availabe for user

--- src/app/pages/read-qr/read-qr.page.ts
+++ src/app/pages/read-qr/read-qr.page.ts
@@ -4,7 +4,8 @@ import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
 import { ToastController } from '@ionic/angular';
 import * as $ from "jquery";
 import { ChangeDetectorRef } from '@angular/core';
-
+import { QRCode, SharedDataService } from 'src/app/data/shared-data.service';
+import { NGSIv2QUERYService, Entity } from 'src/app/data/ngsiv2-query.service'
 /* QR-Code has 3 fields:{"description":"","link":"","code":""} */
 
 @Component({
@@ -18,7 +19,8 @@ export class ReadQRPage implements OnInit {
   scannedCode = null;
   title = 'app';
   isOn = false;
-  constructor(private changeRef: ChangeDetectorRef, private qrScanner: QRScanner,  private toastCtrl: ToastController) {
+  QR_list = [new QRCode(), new QRCode(), new QRCode(), new QRCode()];
+  constructor(private sharedData: SharedDataService, private NGSIv2Query: NGSIv2QUERYService, private changeRef: ChangeDetectorRef, private qrScanner: QRScanner, private toastCtrl: ToastController) {
     document.addEventListener('ionBackButton', (ev) => {
       //console.log(ev)
       if (this.previewCamera) {
@@ -27,6 +29,7 @@ export class ReadQRPage implements OnInit {
         $("ion-app").show(500);
       }
     })
+    this.QR_list = this.sharedData.user_data.qr_code;
   }
   closePreviewCamera() {
     this.previewCamera = false;
@@ -67,5 +70,14 @@ export class ReadQRPage implements OnInit {
   }
   ngOnInit() {
   }
-
+  addQR() {
+    this.NGSIv2Query.getEntity(Entity.NFC,)
+  }
+  delete(device, index) {
+    console.log('delete pos ' + index + " -> " + device.id)
+    var a = $('#item' + index).hide(800, () => {
+      this.QR_list.splice(index, 1);
+      console.log(this.QR_list)
+    })
+  }
 }
