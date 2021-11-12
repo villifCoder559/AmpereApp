a14b5f118a925d4358acf085b4f3e08e39e8fe06
villifCoder559 <francesco.villi@gmail.com>
2021-11-12 20:46:36 +0100

    delete comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { BLE } from '@ionic-native/ble/ngx';
+import { Platform } from '@ionic/angular';
+import { SharedDataService } from '../app/data/shared-data.service'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,8 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  count_emergency_click = 0;
+  constructor() {
+    /* if logged enbale bluetooth */
+  }
 }
