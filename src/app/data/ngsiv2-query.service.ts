b991daa24ce698ebb143db33a0b18f0163c82abd
villifCoder559 <francesco.villi@gmail.com>
2021-11-25 00:02:47 +0100

    add link broker and header

--- src/app/data/ngsiv2-query.service.ts
+++ src/app/data/ngsiv2-query.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, NgModule } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
 
 export enum Entity {
   USERID = 'UserID',
@@ -8,57 +9,98 @@ export enum Entity {
   QR = ''
 }
 @NgModule({
-  providers:[HttpClient]
+  providers: [HttpClient]
 })
 @Injectable({
   providedIn: 'root'
 })
 export class NGSIv2QUERYService {
-  endpoint = 'localhost:1026/v2/entities'
+  endpoint = 'https://broker1.snap4city.org:8080/v1/queryContext?elementid=angelo-prova780'
   httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    headers: new HttpHeaders({ 'Accept': 'application/json', Autentication: this.authService?.keycloak?.token })
   };
-  constructor(public http: HttpClient) { }
+  constructor(public http: HttpClient, private authService: AuthenticationService) { }
+  testAPIGetEntities() {
+    return new Promise((resolve, reject) => {
+      var query = {
+        "entities": [
+          {
+            "type": "Ambiental",
+            "isPattern": false,
+            "id": "mypersonaldatatester-device"
+          }
+        ],
+        "attributes": [
+          "temperature"
+        ],
+      }
+      this.http.post(this.endpoint, query, this.httpOptions).subscribe((value) => {
+        console.log(value)
+        resolve(true)
+      }, (err) => reject(err))
+    })
+  }
+  testRegisterEntity() {
+    return new Promise((resolve, reject) => {
+      var query = {
+        "entities": [
+          {
+            "type": "Ambiental",
+            "isPattern": false,
+            "id": "mypersonaldatatester-device"
+          }
+        ],
+        "attributes": [
+          "temperature"
+        ]
+      }
+      this.http.post(this.endpoint, query, this.httpOptions).subscribe((result) => {
+        resolve(result)
+      }, (err) => {
+        reject(err);
+      })
+    })
+  }
   registerEntity(entity: Entity, userID: number, data) {
     return new Promise((resolve, reject) => {
       resolve(true)
-    //   data['id'] = userID;
-    //   data['type'] = entity;
-    //   this.http.post(this.endpoint, data, this.httpOptions).subscribe((result) => {
-    //     resolve(result)
-    //   }, (err) => {
-    //     reject(err);
-    //   })
+      // data['id'] = userID;
+      // data['type'] = entity;
+      // this.http.post(this.endpoint, data, this.httpOptions).subscribe((result) => {
+      //   resolve(result)
+      // }, (err) => {
+      //   reject(err);
+      // })
     })
   }
   updateEntity(entity: Entity) {
     return new Promise((resolve, reject) => {
       resolve(true)
-    //   this.http.get(this.endpoint + '/' + entity, this.httpOptions).subscribe((result) => {
-    //     resolve(result)
-    //   }, (err) => {
-    //     reject(err);
-    //   })
+      //   this.http.get(this.endpoint + '/' + entity, this.httpOptions).subscribe((result) => {
+      //     resolve(result)
+      //   }, (err) => {
+      //     reject(err);
+      //   })
     })
   }
   updateEntityAttribute(entity: Entity, name_attribute: string, value_attribute) {
     return new Promise((resolve, reject) => {
       resolve(true)
-    //   this.http.put(this.endpoint + '/' + entity + '/' + '/' + name_attribute, +'/' + value_attribute, { headers: { 'Content-Type': 'text/plain' } }).subscribe((entity) => {
-    //     resolve(entity)
-    //   }, (err) => {
-    //     reject(err);
-    //   })
+      //   this.http.put(this.endpoint + '/' + entity + '/' + '/' + name_attribute, +'/' + value_attribute, { headers: { 'Content-Type': 'text/plain' } }).subscribe((entity) => {
+      //     resolve(entity)
+      //   }, (err) => {
+      //     reject(err);
+      //   })
     })
   }
   getEntity(entity: Entity, compact_mode: boolean = true) {
     return new Promise((resolve, reject) => {
       resolve(true)
-    //   this.http.get(this.endpoint + '/' + entity + '/' + compact_mode ? '?options=keyValues' : '', { headers: { 'Accept': 'application/json' } }).subscribe((entity) => {
-    //     resolve(entity)
-    //   }, (err) => {
-    //     reject(err);
-    //   })
+      //   this.http.get(this.endpoint + '/' + entity + '/' + compact_mode ? '?options=keyValues' : '', { headers: { 'Accept': 'application/json' } }).subscribe((entity) => {
+      //     resolve(entity)
+      //   }, (err) => {
+      //     reject(err);
+      //   })
     })
   }
 }
