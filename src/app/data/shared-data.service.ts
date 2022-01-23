4c6dca9526f9de29ddd7dc2895119b1af6b6b8a7
villifCoder559 <francesco.villi@gmail.com>
2022-01-23 17:59:09 +0100

    add local storage

--- src/app/data/shared-data.service.ts
+++ src/app/data/shared-data.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -12,7 +12,7 @@ import { BackgroundMode } from '@awesome-cordova-plugins/background-mode/ngx';
 import { NativeAudio } from '@ionic-native/native-audio/ngx';
 // import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion/ngx';
 import { CountdownConfig, CountdownModule } from 'ngx-countdown';
-
+import { Storage } from '@ionic/storage-angular'
 @NgModule({
   imports: [
     CommonModule,
@@ -21,7 +21,7 @@ import { CountdownConfig, CountdownModule } from 'ngx-countdown';
     CountdownModule
   ],
   providers: [
-    NativeAudio,BackgroundMode]
+    NativeAudio, BackgroundMode]
 })
 export class NFCCode {
   link: string = '';
@@ -74,9 +74,11 @@ export class UserData {
   providedIn: 'root'
 })
 export class SharedDataService {
-  user_data: UserData = new UserData();
+  public user_data: UserData = new UserData();
   gps_enable = false;
-  constructor(private toastCtrl: ToastController, private router: Router, private platform: Platform, private nativeAudio: NativeAudio) {
+  constructor(private storage: Storage, private toastCtrl: ToastController, private router: Router, private platform: Platform, private nativeAudio: NativeAudio) {
+    console.log('contructor')
+    this.storage.create();
     this.platform.ready().then(() => {
       this.nativeAudio.preloadSimple('alert', 'assets/sounds/alert.mp3').then(() => { }, (err) => console.log(err));
       this.nativeAudio.preloadSimple('sendData', 'assets/sounds/send_data.mp3').then(() => { }, (err) => console.log(err));
@@ -98,6 +100,9 @@ export class SharedDataService {
   setUserData(data) {
     this.user_data = data
   }
+  saveData() {
+    this.storage.set('user_data', this.user_data);
+  }
   loadDataUser() {
     // return new Promise((resolve,reject)=>{
     //   this.NGSIv2Query.getEntity(Entity.USERID).then(() => {
@@ -105,41 +110,48 @@ export class SharedDataService {
     //     resolve(true)
     //   }, (err) => reject(err))
     // })
+
     var qr_code = new QRCode();
     qr_code.id = 2;
     qr_code.description = "Call Simon smartphone"
     var nfc_code = new NFCCode();
     nfc_code.id = 1;
     nfc_code.description = "How to take Aspirina"
-    const data: UserData = {
-      address: 'Viale Morgagni 87',
-      allergies: 'gluten',
-      birthdate: '10/25/1947',
-      city: 'Florence',
-      description: 'brown hair, blue eyes',
-      disabilities: [false, true],
-      email: 'email@mail.com',
-      emergency_contacts: [{ number: '8541254732', name: 'Paul Rid' }],
-      ethnicity: 'white',
-      gender: 'male',
-      height: '185',
-      locality: 'Careggi',
-      medications: '',
-      name: 'Wayne',
-      weight: '85',
-      surname: 'Richards',
-      phoneNumber: '2587436910',
-      public_emergency_contacts: { 113: false, "115": false, "118": true },
-      paired_devices: [{ accuray: '15', major: 125, mior: 758, proximity: 'Near', rssi: '-69', tx: '10db', uuid: '51446-54564w-fwfffw4-56d4we5d1e5113d2e1' }],
-      password: '',
-      pin: '0258',
-      purpose: 'Personal safety',
-      qr_code: [qr_code],
-      nfc_code: [nfc_code]
-    }
-    this.user_data = data;
+    this.user_data.address = 'Viale Morgagni 87'
+    this.user_data.allergies = 'gluten'
+    this.user_data.birthdate = '10/25/1947'
+    this.user_data.city = 'Florence'
+    this.user_data.description = 'brown hair blue eyes'
+    this.user_data.disabilities = [false, true]
+    this.user_data.email = 'email@mail.com'
+    this.user_data.emergency_contacts = [{ number: '8541254732', name: 'Paul Rid' }]
+    this.user_data.ethnicity = 'white'
+    this.user_data.gender = 'male'
+    this.user_data.height = '185'
+    this.user_data.locality = 'Careggi'
+    this.user_data.medications = ''
+    this.user_data.name = 'Wayne'
+    this.user_data.weight = '85'
+    this.user_data.surname = 'Richards'
+    this.user_data.phoneNumber = '2587436910'
+    this.user_data.public_emergency_contacts = { 113: false, "115": false, "118": true }
+    this.user_data.paired_devices = []
+    this.user_data.password = ''
+    this.user_data.pin = '0258'
+    this.user_data.purpose = 'Personal safety'
+    this.user_data.qr_code = [qr_code]
+    this.user_data.nfc_code = [nfc_code]
+    console.log('data from service')
+    console.log(this.user_data)
+    this.storage.get('user_data').then((storageData) => {
+      if (storageData != null) {
+        this.user_data = storageData;
+        console.log(storageData)
+      }
+    });
     // this.setUserData(data)
     //this.setIs_logged(true);
+
   }
   goHomepage() {
     //load user data from database
