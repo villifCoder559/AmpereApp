b741237ac5f4d0818d75c9e8d51247e6105b3b52
villifCoder559 <francesco.villi@gmail.com>
2022-01-26 17:26:14 +0100

    add formgroup

--- src/app/pages/callemergencycontacts/callemergencycontacts.module.ts
+++ src/app/pages/callemergencycontacts/callemergencycontacts.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { CallemergencycontactsPageRoutingModule } from './callemergencycontacts-routing.module';
 
 import { CallemergencycontactsPage } from './callemergencycontacts.page';
 import { MaterialModule } from '../../material-module'
+import { CallNumber } from '@awesome-cordova-plugins/call-number/ngx'
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
     MaterialModule,
+    ReactiveFormsModule,
     CallemergencycontactsPageRoutingModule
   ],
-  declarations: [CallemergencycontactsPage]
+  declarations: [CallemergencycontactsPage],
+  providers:[CallNumber]
 })
 export class CallemergencycontactsPageModule {}
--- src/app/pages/callemergencycontacts/callemergencycontacts.page.ts
+++ src/app/pages/callemergencycontacts/callemergencycontacts.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SharedDataService } from 'src/app/data/shared-data.service';
+import { CallNumber } from '@awesome-cordova-plugins/call-number/ngx'
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-callemergencycontacts',
   templateUrl: './callemergencycontacts.page.html',
   styleUrls: ['./callemergencycontacts.page.scss'],
 })
 export class CallemergencycontactsPage implements OnInit {
-
-  constructor() { }
+  formGroup = this._formBuilder.group({
+    contact0name: [''],
+    contact0number: [''],
+    contact1name: [''],
+    contact1number: [''],
+    contact2name: [''],
+    contact2number: [''],
+    contact3name: [''],
+    contact3number: [''],
+    contact4name: [''],
+    contact4number: [''],
+  });
+  constructor(public shared_data:SharedDataService,private callNumber: CallNumber,private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    console.log(this.shared_data.user_data)
   }
-
+  call(contact){
+    this.callNumber.callNumber(contact.number,true).then(()=>{
+      console.log('launch dialer')
+    },err=>alert('error launching dialer'))
+  }
 }
