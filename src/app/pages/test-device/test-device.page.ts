80f70bafd5ab04ea3bd630f57bfa853b76fbc1ec
villifCoder559 <francesco.villi@gmail.com>
2022-01-26 17:27:57 +0100

    add BeaconRegion

--- src/app/pages/test-device/test-device.page.ts
+++ src/app/pages/test-device/test-device.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedDataService, UserData } from '../../data/shared-data.service'
 import { Router } from '@angular/router'
+import { BluetoothService } from 'src/app/data/bluetooth.service';
 @Component({
   selector: 'app-test-device',
   templateUrl: './test-device.page.html',
@@ -8,14 +9,14 @@ import { Router } from '@angular/router'
 })
 export class TestDevicePage implements OnInit {
   
-  constructor(public shared_data: SharedDataService, private router: Router) {
+  constructor(public shared_data: SharedDataService, private router: Router,private bluetoothService:BluetoothService) {
   }
   ngOnInit() {
   }
   go_to_deviceSettings() {
     this.router.navigateByUrl('/profile/menu/profile', { replaceUrl: true, state: { page: 6 } })
   }
-  testDevice(){
-    alert('')
+  isBeaconInRegion(index){
+    this.bluetoothService.checkRangeBeaconsInRegion(index);
   }
 }
