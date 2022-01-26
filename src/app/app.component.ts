9ad25836a46910b8050837135d0e00bd9a13acbd
villifCoder559 <francesco.villi@gmail.com>
2022-01-26 17:25:37 +0100

    add pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,16 @@ import { BLE } from '@ionic-native/ble/ngx';
 import { Platform } from '@ionic/angular';
 import { SharedDataService } from '../app/data/shared-data.service'
 import { BluetoothService } from '../app/data/bluetooth.service'
+import { Pipe, PipeTransform} from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
 
+@Pipe({ name: 'safe' })
+export class SafePipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) { }
+  transform(url) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
