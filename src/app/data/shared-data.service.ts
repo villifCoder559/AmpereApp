45504e03130fb5aabc2f782ab44986e126011e89
villifCoder559 <francesco.villi@gmail.com>
2022-01-28 18:35:09 +0100

    fix bug for testing

--- src/app/data/shared-data.service.ts
+++ src/app/data/shared-data.service.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule, Platform, ToastController } from '@ionic/angular';
 // import { LocationAccuracy } from '@ionic-native/location-accuracy/ngx';
-// import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 // import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
 import { BackgroundMode } from '@awesome-cordova-plugins/background-mode/ngx';
 // import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
@@ -14,6 +14,7 @@ import { NativeAudio } from '@ionic-native/native-audio/ngx';
 import { CountdownConfig, CountdownModule } from 'ngx-countdown';
 import { Storage } from '@ionic/storage-angular'
 import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { AuthenticationService } from '../services/authentication.service'
 /**fix logout */
 @NgModule({
   imports: [
@@ -84,7 +85,7 @@ export class UserData {
 export class SharedDataService {
   public user_data: UserData = new UserData();
   gps_enable = false;
-  constructor(private backgroundMode: BackgroundMode, private storage: Storage, private toastCtrl: ToastController, private router: Router, private platform: Platform, private nativeAudio: NativeAudio) {
+  constructor(private auth: AuthenticationService, private backgroundMode: BackgroundMode, private storage: Storage, private toastCtrl: ToastController, private router: Router, private platform: Platform, private nativeAudio: NativeAudio) {
     //console.log('contructor')
     this.storage.create();
     this.platform.ready().then(() => {
@@ -111,67 +112,69 @@ export class SharedDataService {
     //console.log('data from service')
     //console.log(this.user_data)
     return new Promise((resolve, reject) => {
-      this.storage.get('user_data').then((storageData) => {
-        if (storageData != null) {
-          console.log('TRUE_BLOCK')
-          console.log('LOADINGDATA')
-          this.user_data = storageData;
-          this.user_data.name = 'TEST'
-          resolve(true);
-        }
-        else {
-          console.log('ELSE_BLOCK')
-          var qr_code = new QRCode();
-          qr_code.id = 2;
-          qr_code.description = "Call Simon smartphone"
-          var nfc_code = new NFCCode();
-          nfc_code.id = 1;
-          nfc_code.description = "How to take Aspirina"
-          this.user_data.nickname = 'KL15'
-          this.user_data.address = 'Viale Morgagni 87'
-          this.user_data.allergies = 'gluten'
-          this.user_data.birthdate = '10/25/1947'
-          this.user_data.city = 'Florence'
-          this.user_data.description = 'brown hair blue eyes'
-          this.user_data.disabilities = [false, true]
-          this.user_data.email = 'email@mail.com'
-          this.user_data.emergency_contacts = [new Emergency_Contact('Paul', 'Rid', '785232145202')]
-          this.user_data.ethnicity = 'white'
-          this.user_data.gender = 'male'
-          this.user_data.height = '185'
-          this.user_data.locality = 'Careggi'
-          this.user_data.medications = ''
-          this.user_data.name = 'Wayne'
-          this.user_data.weight = '85'
-          this.user_data.surname = 'Richards'
-          this.user_data.phoneNumber = '2587436910'
-          this.user_data.public_emergency_contacts = { 112: false, "115": false, "118": true }
-          this.user_data.paired_devices = []
-          this.user_data.pin = '0258'
-          this.user_data.purpose = 'Personal safety'
-          this.user_data.qr_code = [qr_code]
-          this.user_data.nfc_code = [nfc_code];
-          resolve(false)
-        }
-      }, err => console.log(err));
-    })
-  }
-  // goHomepage() {
-  //   //load user data from database
-  //   this.loadDataUser()
-  //   this.router.navigateByUrl('/profile/menu/homepage', { replaceUrl: true });
-  // }
-  showAlert() {
-    //take bluetooth signal, create handler that takes the signal
-    this.nativeAudio.play('alert')
-    this.router.navigateByUrl('/show-alert', { replaceUrl: true })
-    //this.sendEmergency();
-  }
+      if (this.auth.isAuthenticated.getValue()) {
+        this.storage.get('user_data').then((storageData) => {
+          if (storageData != null) {
+            console.log('TRUE_BLOCK')
+            console.log('LOADINGDATA')
+            this.user_data = storageData;
+            this.user_data.name = 'TEST'
+            resolve(true);
+          }
+          else {
+            console.log('ELSE_BLOCK')
+            var qr_code = new QRCode();
+            qr_code.id = 2;
+            qr_code.description = "Call Simon smartphone"
+            var nfc_code = new NFCCode();
+            nfc_code.id = 1;
+            nfc_code.description = "How to take Aspirina"
+            this.user_data.nickname = 'KL15'
+            this.user_data.address = 'Viale Morgagni 87'
+            this.user_data.allergies = 'gluten'
+            this.user_data.birthdate = '10/25/1947'
+            this.user_data.city = 'Florence'
+            this.user_data.description = 'brown hair blue eyes'
+            this.user_data.disabilities = [false, false]
+            this.user_data.email = 'email@mail.com'
+            this.user_data.emergency_contacts = [new Emergency_Contact('Paul', 'Rid', '785232145202')]
+            this.user_data.ethnicity = 'white'
+            this.user_data.gender = 'male'
+            this.user_data.height = '185'
+            this.user_data.locality = 'Careggi'
+            this.user_data.medications = ''
+            this.user_data.name = 'Wayne'
+            this.user_data.weight = '85'
+            this.user_data.surname = 'Richards'
+            this.user_data.phoneNumber = '2587436910'
+            this.user_data.public_emergency_contacts = { 112: false, "115": false, "118": true }
+            this.user_data.paired_devices = []
+            this.user_data.pin = '0258'
+            this.user_data.purpose = 'Personal safety'
+            this.user_data.qr_code = [qr_code]
+            this.user_data.nfc_code = [nfc_code];
+            resolve(false)
+          }
+        }, err => console.log(err));
+      }
+  })
+}
+// goHomepage() {
+//   //load user data from database
+//   this.loadDataUser()
+//   this.router.navigateByUrl('/profile/menu/homepage', { replaceUrl: true });
+// }
+showAlert() {
+  //take bluetooth signal, create handler that takes the signal
+  this.nativeAudio.play('alert')
+  this.router.navigateByUrl('/show-alert', { replaceUrl: true })
+  //this.sendEmergency();
+}
 
-  enableAllBackgroundMode() {
-    console.log('enableBackgroundMode')
-    this.backgroundMode.enable();
-    this.backgroundMode.overrideBackButton();
-    this.backgroundMode.disableWebViewOptimizations();
-  }
+enableAllBackgroundMode() {
+  console.log('enableBackgroundMode')
+  this.backgroundMode.enable();
+  this.backgroundMode.overrideBackButton();
+  this.backgroundMode.disableWebViewOptimizations();
+}
 }
