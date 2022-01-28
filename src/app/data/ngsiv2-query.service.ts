7f5fb61cd032ef69a4dfa37ca72ce80ad47d637f
villifCoder559 <francesco.villi@gmail.com>
2022-01-28 18:34:26 +0100

    add get and set queries

--- src/app/data/ngsiv2-query.service.ts
+++ src/app/data/ngsiv2-query.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, NgModule } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
 import { Snap4CityService } from '../data/snap4-city.service'
+import { SharedDataService } from '../data/shared-data.service'
 export enum Entity {
   USERID = 'UserID',
   EVENT = 'Event',
@@ -16,38 +17,180 @@ export enum Entity {
 })
 export class NGSIv2QUERYService {
   endpoint = 'https://https://iot-app.snap4city.org/orionfilter/orionAMPERE-UNIFI:443/v1'
-  constructor(public http: HttpClient, private authService: AuthenticationService, private adapterS4C: Snap4CityService) { }
-  testWriteAPI() {
+  constructor(private shared_data: SharedDataService, public http: HttpClient, private authService: AuthenticationService, private adapterS4C: Snap4CityService) { }
+  sendQRNFCEvent(qridornfc, action, dateObserved, identifier, latitude, longitude) {
+    var id = this.authService.username;
+    $.ajax({
+      url: "https://iot-app.snap4city.org/orionfilter/orionAMPERE-UNIFI/v2/entities/" + id + "QR-NFC-Event/attrs?elementid=" + id + "QR-NFC-Event&type=QR-NFC-Event",
+      headers: {
+        'Authorization': 'Bearer ' + this.authService.keycloak.refreshToken,
+        'Content-Type': 'application/json'
+      },
+      type: "PATCH",
+      data: {
+        "id": id,
+        "type": "QR-NFC-Event",
+        "QRIDorNFC": { "type": "string", "value": qridornfc },
+        "action": { "type": "string", "value": action },
+        "dateObserved": { "type": "string", "value": dateObserved },
+        "identifier": { "type": "string", "value": identifier },
+        "latitude": { "type": "float", "value": latitude },
+        "longitude": { "type": "float", "value": longitude }
+      },
+      async: true,
+      dataType: 'json',
+      success: function (mydata) {
+        if (mydata.status != 'ko') {
+          console.log('Data')
+          console.log(mydata);
+        }
+        else {
+          console.log('error')
+        }
+      },
+      error: function (err) {
+        console.log("Insert values pool KO");
+        console.log(err);
+      }
+    });
+  }
+  sendUserProfile(dateObserved) {
     return new Promise((resolve, reject) => {
-      var data = this.adapterS4C.getQRNFCEventPayload();
-      console.log('test1')
-      //console.log(JSON.stringify(data))
+      var data: any = this.shared_data.user_data;
+      var id = this.authService.username;
+      data.id = id;
+      data.type = 'AmpereUserProfile'
+      data.dateObserved = dateObserved
       $.ajax({
-        url: "https://iot-app.snap4city.org/orionfilter/orionAMPERE-UNIFI/v2/entities/ampereuser1QRNFCEvent?elementid=ampereuser1QRNFCEvent&type=EventQRNFC",
+        url: "https://iot-app.snap4city.org/orionfilter/orionAMPERE-UNIFI/v2/entities/" + id + "AmpereUserProfile/attrs?elementid=" + id + "AmpereUserProfile&type=AmpereUserProfile",
         headers: {
-          'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJOZVBpSFRvREtibWZzbl9hREtETGpGTHFKQXluTXNNWjZjS1lMeGRoS29zIn0.eyJqdGkiOiI0Yjg0YjBlNy02ZGFhLTRlOGEtYTliNi1kMzUwZThiNTU3ODEiLCJleHAiOjE2NDQzMjk2ODQsIm5iZiI6MCwiaWF0IjoxNjQzMDE1ODkxLCJpc3MiOiJodHRwczovL3d3dy5zbmFwNGNpdHkub3JnL2F1dGgvcmVhbG1zL21hc3RlciIsImF1ZCI6InBocC1pb3QtZGlyZWN0b3J5Iiwic3ViIjoiZGI1ZmU3NzYtMzA1YS00NDIwLWIyNDctODQzZmY1ZmFlMGNiIiwidHlwIjoiUmVmcmVzaCIsImF6cCI6InBocC1pb3QtZGlyZWN0b3J5Iiwibm9uY2UiOiJlNmRiZGEzNTBjMzllNjk2ZTlhNTYzMzc2NTRmMDg2MiIsImF1dGhfdGltZSI6MCwic2Vzc2lvbl9zdGF0ZSI6ImQ4NDc4NzVjLTc1MzQtNDVmZi04OTFhLWIyNDMyNzIzZDU3MSIsInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJ1bWFfYXV0aG9yaXphdGlvbiIsIk1hbmFnZXIiXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19fQ.fukRqbFYq8OXHa70Yy3_PcJCxVw-dYh9vW1njAivcenGjfzNlEvS912S8lKSnhfzZV4xzQURDIwiLlSoZeVpsd5owbFd7icXPpbf_RqHhB4d-lPZieL7nD5zyVrKmuXcdxQC2D_YmTKdUc-MgpRXLbH9nSHGkzfne4_uCwy8xVsLK6zCVuW1AZI0Zoud6aK3ARdRMyod4V170Tf4aTjnvL0QhZq8PUlF7ogvk2pb4VhMYUfYECZHtbK9uxge6Fal9H4fgtpPHS8PxRJ8yLlq06fDo-jvdqn8EZfLMScWridpGYOATPLSVTjlXPnOOA_1RNwB1b7w26pygrtBfRrryAeyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJOZVBpSFRvREtibWZzbl9hREtETGpGTHFKQXluTXNNWjZjS1lMeGRoS29zIn0.eyJqdGkiOiI0Yjg0YjBlNy02ZGFhLTRlOGEtYTliNi1kMzUwZThiNTU3ODEiLCJleHAiOjE2NDQzMjk2ODQsIm5iZiI6MCwiaWF0IjoxNjQzMDE1ODkxLCJpc3MiOiJodHRwczovL3d3dy5zbmFwNGNpdHkub3JnL2F1dGgvcmVhbG1zL21hc3RlciIsImF1ZCI6InBocC1pb3QtZGlyZWN0b3J5Iiwic3ViIjoiZGI1ZmU3NzYtMzA1YS00NDIwLWIyNDctODQzZmY1ZmFlMGNiIiwidHlwIjoiUmVmcmVzaCIsImF6cCI6InBocC1pb3QtZGlyZWN0b3J5Iiwibm9uY2UiOiJlNmRiZGEzNTBjMzllNjk2ZTlhNTYzMzc2NTRmMDg2MiIsImF1dGhfdGltZSI6MCwic2Vzc2lvbl9zdGF0ZSI6ImQ4NDc4NzVjLTc1MzQtNDVmZi04OTFhLWIyNDMyNzIzZDU3MSIsInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJ1bWFfYXV0aG9yaXphdGlvbiIsIk1hbmFnZXIiXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19fQ.fukRqbFYq8OXHa70Yy3_PcJCxVw-dYh9vW1njAivcenGjfzNlEvS912S8lKSnhfzZV4xzQURDIwiLlSoZeVpsd5owbFd7icXPpbf_RqHhB4d-lPZieL7nD5zyVrKmuXcdxQC2D_YmTKdUc-MgpRXLbH9nSHGkzfne4_uCwy8xVsLK6zCVuW1AZI0Zoud6aK3ARdRMyod4V170Tf4aTjnvL0QhZq8PUlF7ogvk2pb4VhMYUfYECZHtbK9uxge6Fal9H4fgtpPHS8PxRJ8yLlq06fDo-jvdqn8EZfLMScWridpGYOATPLSVTjlXPnOOA_1RNwB1b7w26pygrtBfRrryA',
+          'Authorization': 'Bearer ' + this.authService.keycloak.refreshToken,
+          'Content-Type': 'application/json'
+        },
+        type: "PATCH",
+        data: data,
+        async: true,
+        dataType: 'json',
+        success: function (mydata) {
+          if (mydata.status != 'ko') {
+            console.log('Data')
+            console.log(mydata);
+          }
+          else {
+            console.log('error')
+          }
+        },
+        error: function (err) {
+          console.log("Insert values pool KO");
+          console.log(err);
+        }
+      });
+    })
+  }
+  sendEvent(accelX, accelY, accelZ, dateObserved, deviceID, evolution, quote, status, velocity, latitude, longitude) {
+    return new Promise((resolve, reject) => {
+      var id = this.authService.username;
+      $.ajax({
+        url: "https://iot-app.snap4city.org/orionfilter/orionAMPERE-UNIFI/v2/entities/" + id + "Event/attrs?elementid=" + id + "Event&type=Event",
+        headers: {
+          'Authorization': 'Bearer ' + this.authService.keycloak.refreshToken,
           'Content-Type': 'application/json'
         },
         type: "PATCH",
         data: {
-          payload: {
-            action: {
-              value: 'test1234',
-              type: 'string'
-            },
-            dataObserved: {
-              value: new Date().toISOString(),
-              type: 'string'
-            },
-            identifier: {
-              value: '85',
-              type: 'string'
-            },
-            QRIDorNFC: {
-              value: 'NFC',
-              type: 'string'
-            }
+          "id": id,
+          "type": "Event",
+          "accelX": { "type": "string", "value": accelX },
+          "accelZ": { "type": "string", "value": accelZ },
+          "accelY": { "type": "string", "value": accelY },
+          "dateObserved": { "type": "string", "value": dateObserved },
+          "deviceID": { "type": "string", "value": deviceID },
+          "evolution": { "type": "string", "value": evolution },
+          "quote": { "type": "string", "value": quote },
+          "status": { "type": "string", "value": status },
+          "velocity": { "type": "string", "value": velocity },
+          "latitude": { "type": "float", "value": latitude },
+          "longitude": { "type": "float", "value": longitude }
+        },
+        async: true,
+        dataType: 'json',
+        success: function (mydata) {
+          if (mydata.status != 'ko') {
+            console.log('Data')
+            console.log(mydata);
+            resolve(true)
           }
+          else {
+            console.log('error')
+            reject(false)
+          }
+        },
+        error: function (err) {
+          console.log("Insert values pool KO");
+          console.log(err);
+        }
+      });
+    })
+  }
+  sendQRNFCDictionary(qridornfc, action, dateObserved, identifier, identifierCode, latitude, longitude) {
+    var id = this.authService.username;
+    $.ajax({
+      url: "https://iot-app.snap4city.org/orionfilter/orionAMPERE-UNIFI/v2/entities/" + id + "QR-NFC-Event/attrs?elementid=" + id + "QR-NFC-Event&type=QR-NFC-Event",
+      headers: {
+        'Authorization': 'Bearer ' + this.authService.keycloak.refreshToken,
+        'Content-Type': 'application/json'
+      },
+      type: "PATCH",
+      data: {
+        "id": id,
+        "type": "QR-NFC-Event",
+        "QRIDorNFC": { "type": "string", "value": qridornfc },
+        "action": { "type": "string", "value": action },
+        "dateObserved": { "type": "string", "value": dateObserved },
+        "identifier": { "type": "string", "value": identifier },
+        "identifiercode": { "type": "string", "value": identifierCode },
+        "latitude": { "type": "float", "value": latitude },
+        "longitude": { "type": "float", "value": longitude }
+      },
+      async: true,
+      dataType: 'json',
+      success: function (mydata) {
+        if (mydata.status != 'ko') {
+          console.log('Data')
+          console.log(mydata);
+        }
+        else {
+          console.log('error')
+        }
+      },
+      error: function (err) {
+        console.log("Insert values pool KO");
+        console.log(err);
+      }
+    });
+  }
+  testWriteAPI(type) {
+    return new Promise((resolve, reject) => {
+      var data = this.adapterS4C.getQRNFCEventPayload();
+      var username = this.authService.username;
+      console.log('test1')
+      //console.log(JSON.stringify(data))
+      $.ajax({
+        url: "https://iot-app.snap4city.org/orionfilter/orionAMPERE-UNIFI/v2/entities/" + username + type + "/attrs?elementid=" + username + type + "&type=" + type,
+        headers: {
+          'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJOZVBpSFRvREtibWZzbl9hREtETGpGTHFKQXluTXNNWjZjS1lMeGRoS29zIn0.eyJqdGkiOiI5OTJjMTQyYS00YjhiLTQ3YzEtODBkNi00ODg5NGFiYWJkYmYiLCJleHAiOjE2NDQzMjk2ODQsIm5iZiI6MCwiaWF0IjoxNjQzMzY1NTE2LCJpc3MiOiJodHRwczovL3d3dy5zbmFwNGNpdHkub3JnL2F1dGgvcmVhbG1zL21hc3RlciIsImF1ZCI6InBocC1pb3QtZGlyZWN0b3J5Iiwic3ViIjoiZGI1ZmU3NzYtMzA1YS00NDIwLWIyNDctODQzZmY1ZmFlMGNiIiwidHlwIjoiUmVmcmVzaCIsImF6cCI6InBocC1pb3QtZGlyZWN0b3J5Iiwibm9uY2UiOiI5OGUwYzg4NjdjOTQ5MTBjM2VmOTYzZTAzY2U3MzhjZCIsImF1dGhfdGltZSI6MCwic2Vzc2lvbl9zdGF0ZSI6ImQ4NDc4NzVjLTc1MzQtNDVmZi04OTFhLWIyNDMyNzIzZDU3MSIsInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJ1bWFfYXV0aG9yaXphdGlvbiIsIk1hbmFnZXIiXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19fQ.KVVMMgvS8KGcLvnjQ3FnpBReDLAtEUSyrsjy-G-tNi7N3UOPLuMhOJ_Vp0vO4mjl4cYMCDk2-pw30YtVdSX2-tlchDf73cqhRCWslNTFASb3r0nWGJridk64zgH152EE-NsABPo010-NxPpgH8BbKLurOC9GVOjgOLEy5RVJWvI6K-W18cWIl8X3Qd7kUWGLvDPB3zY7QIcpNGFy84nUsaJewHqoxQyYIysUNHpNGFajw8grpGWXy-swBvpnTG2AM7KQ-0dYhI2Nzbp0ZSOntwo_Ftc99MNzWxWtCIYZG79tbTa4nJwxUdx-7SnaBYMFnKYraS4kl8PfzJ2jJyOaGg',
+          'Content-Type': 'application/json'
+        },
+        type: "PATCH",
+        data: {
+          "id": "ampereuser1QR-NFC-Event",
+          "type": "QR-NFC-Event",
+          "QRIDorNFC": { "type": "string", "value": "QR" },
+          "action": { "type": "string", "value": "a" },
+          "dateObserved": { "type": "string", "value": "2022-01-15T15:05:44.412Z" },
+          "identifier": { "type": "string", "value": "78" },
+          "latitude": { "type": "float", "value": "43" },
+          "longitude": { "type": "float", "value": "12" }
         },
         async: true,
         dataType: 'json',
@@ -69,18 +212,16 @@ export class NGSIv2QUERYService {
       });
     })
   }
-  checkValidityToken() {
-
-  }
-  testAPIGetEntities() {
+  getEntity(type) {
     return new Promise((resolve, reject) => {
       var data = this.adapterS4C.getQRNFCEventPayload();
       console.log('test1')
+      var username = this.authService.username;
       //console.log(JSON.stringify(data))
       $.ajax({
-        url: "https://iot-app.snap4city.org/orionfilter/orionAMPERE-UNIFI/v2/entities/ampereuser1QR-NFC-Event?elementid=ampereuser1QR-NFC-Event&type=QR-NFC-Event",
+        url: "https://iot-app.snap4city.org/orionfilter/orionAMPERE-UNIFI/v2/entities/" + username + type + "?elementid=" + username + type + "&type=" + type,
         type: "GET",
-        headers:{
+        headers: {
           'Content-Type': 'application/json'
         },
         async: true,
@@ -103,6 +244,7 @@ export class NGSIv2QUERYService {
       });
     })
   }
+  /* Using device.php */
   getDeviceData() {
     return new Promise((resolve, reject) => {
       $.ajax({
@@ -133,46 +275,5 @@ export class NGSIv2QUERYService {
       })
     })
   }
-  registerEntity(entity: Entity, userID: number, data) {
-    return new Promise((resolve, reject) => {
-      resolve(true)
-      // data['id'] = userID;
-      // data['type'] = entity;
-      // this.http.post(this.endpoint, data, this.httpOptions).subscribe((result) => {
-      //   resolve(result)
-      // }, (err) => {
-      //   reject(err);
-      // })
-    })
-  }
-  updateEntity(entity: Entity) {
-    return new Promise((resolve, reject) => {
-      resolve(true)
-      //   this.http.get(this.endpoint + '/' + entity, this.httpOptions).subscribe((result) => {
-      //     resolve(result)
-      //   }, (err) => {
-      //     reject(err);
-      //   })
-    })
-  }
-  updateEntityAttribute(entity: Entity, name_attribute: string, value_attribute) {
-    return new Promise((resolve, reject) => {
-      resolve(true)
-      //   this.http.put(this.endpoint + '/' + entity + '/' + '/' + name_attribute, +'/' + value_attribute, { headers: { 'Content-Type': 'text/plain' } }).subscribe((entity) => {
-      //     resolve(entity)
-      //   }, (err) => {
-      //     reject(err);
-      //   })
-    })
-  }
-  getEntity(entity: Entity, compact_mode: boolean = true) {
-    return new Promise((resolve, reject) => {
-      resolve(true)
-      //   this.http.get(this.endpoint + '/' + entity + '/' + compact_mode ? '?options=keyValues' : '', { headers: { 'Accept': 'application/json' } }).subscribe((entity) => {
-      //     resolve(entity)
-      //   }, (err) => {
-      //     reject(err);
-      //   })
-    })
-  }
+
 }
