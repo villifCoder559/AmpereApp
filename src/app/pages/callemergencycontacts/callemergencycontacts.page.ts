67104f599c2311ea42c5f29f39ef7b62e4d40034
villifCoder559 <francesco.villi@gmail.com>
2022-01-28 10:09:03 +0100

    fix bug and add setting button

--- src/app/pages/callemergencycontacts/callemergencycontacts.page.ts
+++ src/app/pages/callemergencycontacts/callemergencycontacts.page.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedDataService } from 'src/app/data/shared-data.service';
 import { CallNumber } from '@awesome-cordova-plugins/call-number/ngx'
-
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-callemergencycontacts',
   templateUrl: './callemergencycontacts.page.html',
   styleUrls: ['./callemergencycontacts.page.scss'],
 })
 export class CallemergencycontactsPage implements OnInit {
-  constructor(public shared_data:SharedDataService,private callNumber: CallNumber) { }
+  formGroup = this._formBuilder.group({
+    contactName:[],
+    contactSurname:[],
+    contactNumber:[]
+  });
+  constructor(private router:Router,public shared_data:SharedDataService,private callNumber: CallNumber,private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
     console.log(this.shared_data.user_data)
@@ -17,5 +23,8 @@ export class CallemergencycontactsPage implements OnInit {
     this.callNumber.callNumber(contact.number,true).then(()=>{
       console.log('launch dialer')
     },err=>alert('error launching dialer'))
+  }
+  go_to_deviceSettings(){
+    this.router.navigateByUrl('/profile/menu/profile', { replaceUrl: true, state: { page: 3 } })
   }
 }
