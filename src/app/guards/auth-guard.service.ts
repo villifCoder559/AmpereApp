ec90ca7ccd80d0f581556e610278aa6d6e1d5ca0
villifCoder559 <francesco.villi@gmail.com>
2022-02-11 21:38:18 +0100

    add subscription for logout

--- src/app/guards/auth-guard.service.ts
+++ src/app/guards/auth-guard.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 import { SharedDataService } from '../data/shared-data.service';
-import {AuthenticationService} from '../services/authentication.service'
+import { AuthenticationService } from '../services/authentication.service'
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
 
-  constructor(private authService:AuthenticationService,private router: Router) { }
+  constructor(private authService: AuthenticationService, private router: Router) { }
   canActivate(route: ActivatedRouteSnapshot): boolean {
-    console.log(route)
+    if (this.authService.isAuthenticated.observers.length == 0)
+      this.activateSubscription();
     if (this.authService.isAuthenticated.getValue())
       return true;
     else {
@@ -18,5 +19,12 @@ export class AuthGuardService implements CanActivate {
         return false
       }, (err) => { return false })
     }
+  }
+  activateSubscription() {
+    this.authService.isAuthenticated.subscribe(() => {
+      console.log('subs triggered')
+      if (!this.authService.isAuthenticated.getValue())
+        this.router.navigateByUrl("", { replaceUrl: true })
+    })
   }
 }
