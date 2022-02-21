429a18cb16a53e53c8c7097f1f03267746c1fcde
villifCoder559 <francesco.villi@gmail.com>
2022-02-21 11:15:56 +0100

    refactor code

--- src/app/pages/read-nfc/read-nfc.page.ts
+++ src/app/pages/read-nfc/read-nfc.page.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NFC, Ndef } from '@ionic-native/nfc/ngx'
 import { Platform, ToastController } from '@ionic/angular';
-import { DeviceType, NFCCode, QRNFCEvent, SharedDataService, typeChecking } from 'src/app/data/shared-data.service';
+import { DeviceType, QRNFCEvent, SharedDataService, typeChecking } from 'src/app/data/shared-data.service';
 import { NGSIv2QUERYService } from 'src/app/data/ngsiv2-query.service';
 import { Snap4CityService } from 'src/app/data/snap4-city.service';
 import { LoadingController } from '@ionic/angular';
+import { ReadingCodeService } from 'src/app/data/reading-code.service';
 
 @Component({
   selector: 'app-read-nfc',
@@ -16,88 +17,37 @@ export class ReadNFCPage implements OnInit {
   NFC_data = '';
   NFC_enable = false;
   scannedCode = null;
-  constructor(private s4c: Snap4CityService, private NGSIv2Query: NGSIv2QUERYService, public sharedData: SharedDataService, private toastCtrl: ToastController, private nfc: NFC, private ndef: Ndef, private platform: Platform) {
+  constructor(private readCode: ReadingCodeService, public sharedData: SharedDataService,  private nfc: NFC,  private platform: Platform) {
     console.log(this.sharedData.user_data)
-  }
-  addNFC() {
-
   }
   ngOnInit() {
     this.nfc.enabled().then(() => {
       this.NFC_enable = true;
       this.read_NFC();
-    }, err => this.create_message('Error :' + err))
-    async () => {
-      this.sharedData.user_data.nfc_code.forEach((element) => {
-        this.NGSIv2Query.getEntity('QRNFCDictionary' + element.id, 'DictionaryOfQRNFC').then((data: any) => {
-          if (data.QRIDorNFC.toUpperCase() == 'NFC') {
-            this.sharedData.user_data.nfc_code[data.identifier].action = data.action;
-          }
-        })
-      })
-    }
+    }, err => this.sharedData.createToast('Error :' + err))
   }
   async read_NFC() {
     if (this.platform.is('android')) {
       let flags = this.nfc.FLAG_READER_NFC_A | this.nfc.FLAG_READER_NFC_V;
       var readerMode = this.nfc.readerMode(flags).subscribe(
         tag => {
-          console.log(tag)
-          this.scannedCode = JSON.stringify(tag)
-          window.open(tag.ndefMessage.toString())
-          var id = parseInt(tag.ndefMessage.toString());
-          if (!isNaN(id)) {
-            if (this.sharedData.checkIDValidityNFCorQR(typeChecking.NFC_CODE, id)) {
-              this.sharedData.createToast('Valid NFC');
-              this.NGSIv2Query.getEntity('QRNFCDictionary' + id, 'QRNFCDictionary').then((response: any) => {
-                var action: string = response.action.value;
-                this.s4c.createDevice(DeviceType.QR_NFC_EVENT).then(() => {
-                  this.NGSIv2Query.sendQRNFCEvent(new QRNFCEvent('NFC', action, response.identifier.value));
-                  window.open('https://' + action, '_system', 'location=yes')
-                }, err => {
-                  console.log(err);
-                })
-              }, (err) => {
-                alert(err.msg);
-              })
-            }
-            else {
-              this.sharedData.createToast('Permission denied!')
-              //reject('Permission denied')
-            }
-          }
-          else {
-            alert('Not valid NFC')
-          }
+          var text = this.nfc.bytesToString(tag.ndefMessage[0].payload).substring(3);
+          console.log(text);
+          this.sharedData.presentLoading('Getting info from server').then(() => {
+            this.readCode.readURLFromServer(text, typeChecking.NFC_CODE).then(() => {
+              this.sharedData.createToast('QR scanned succesfully')
+              this.sharedData.dismissLoading();
+            }, err => {
+              this.sharedData.createToast(err?.msg)
+              this.sharedData.dismissLoading();
+            })
+          })
         },
-        err => this.create_message('Error reading tag: ' + err)
+        err => this.sharedData.createToast('Error reading tag: ' + err)
       );
     }
-    else if (this.platform.is('ios')) {
-      try {
-        let tag = await this.nfc.scanNdef();
-        this.scannedCode = JSON.stringify(tag);
-      } catch (err) {
-        alert('Error reading tag ' + err.msg);
-      }
-    }
   }
-
-  async create_message(txt: string) {
-    let toast = await this.toastCtrl.create({
-      header: txt,
-      duration: 2000
-    })
-    toast.present();
-  }
-  delete(device, index) {
-    console.log('delete pos ' + index + " -> " + device.id)
-    $('#item' + index).hide(400, () => {
-      this.sharedData.user_data.nfc_code.splice(index, 1);
-      console.log(this.sharedData.user_data)
-    })
-  }
-  ngOnDestroy() {
-
+  ngOnDestroy(){
+    this.nfc.close()
   }
 }
