d44e72a8c3dec05791b3ebc3ac691a04626f41b0
villifCoder559 <francesco.villi@gmail.com>
2022-02-21 11:15:25 +0100

    update pages

--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -13,34 +13,45 @@ export class MenuPage implements OnInit {
   pages = [
     {
       title: 'Homepage',
-      url: '/profile/menu/homepage'
+      url: '/profile/menu/homepage',
+      icon:'home'
     },
     {
       title: 'Emergency contacts',
-      url: '/profile/menu/callemergencycontacts'
+      url: '/profile/menu/callemergencycontacts',
+      icon:'people'
     },
     {
       title: 'Webpage',
-      url: '/profile/menu/webpage'
+      url: '/profile/menu/webpage',
+      icon:'earth'
     },
     {
       title: 'Devices Status',
-      url: '/profile/menu/testAlert'
+      url: '/profile/menu/testAlert',
+      icon:'phone-portrait'
     },
     {
       title: 'Term of Use',
-      url: '/profile/menu/termOfUse'
+      url: '/profile/menu/termOfUse',
+      icon:'document'
     },
     {
       title: 'Privacy Policy',
-      url: '/profile/menu/privacyPolicy'
+      url: '/profile/menu/privacyPolicy',
+      icon:'document'
+    },
+    {
+      title: 'Logout',
+      url: '',
+      icon:"log-out"
     }
   ]
   selectedPath = ''
   constructor(private ctrlMenu: MenuController, private router: Router, private authService: AuthenticationService, public shared_data: SharedDataService) {
     console.log('costruttore')
     this.router.events.subscribe((event: RouterEvent) => {
-      this.selectedPath = event.url
+      this.selectedPath = event.url;
     })
   }
   go_back() {
