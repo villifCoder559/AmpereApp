97587a93a9fc4f23cd2a1b8c4d1f79579614e5e5
villifCoder559 <francesco.villi@gmail.com>
2022-02-21 11:18:38 +0100

    refactor code

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -3,6 +3,7 @@ import { BehaviorSubject, from, Observable, Subject } from 'rxjs';
 import * as Keycloak from 'keycloak-ionic/keycloak';
 import { SharedDataService, UserData } from '../data/shared-data.service';
 import { Router } from '@angular/router';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ import { Router } from '@angular/router';
 export class AuthenticationService {
   isAuthenticated: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(null);
   public keycloak: Keycloak.KeycloakInstance;
-  constructor(private shareData: SharedDataService, private router: Router) {
+  constructor(private localNotifications: LocalNotifications, private shareData: SharedDataService, private router: Router) {
     //this.loadToken();
   }
 
@@ -56,9 +57,24 @@ export class AuthenticationService {
         .then((autentication) => {
           if (autentication) {
             this.keycloak.loadUserProfile().then((info: any) => {
+              this.keycloak.onTokenExpired = () => {
+                this.keycloak.updateToken(30).then((refreshed) => {
+                  if (refreshed)
+                    console.log('token refreshed correctly')
+                  else
+                    console.log('Token still valid')
+                }, err => {
+                  console.log(err)
+                  this.localNotifications.schedule({
+                    text: 'Token expired, login to continue using the app!'
+                  })
+                  this.logout();
+                })
+              }
               this.shareData.user_data.id = info.username;
               console.log(this.shareData.user_data.id)
               console.log('token ' + this.keycloak.token)
+              console.log(info)
               resolve(true);
             })
           }
@@ -74,6 +90,7 @@ export class AuthenticationService {
         })
     })
   }
+
   logout() {
     //this.bluetoothService.disableAll();
     window.localStorage.removeItem('TOKEN_KEY');
@@ -82,6 +99,5 @@ export class AuthenticationService {
       this.keycloak.logout().then(() => this.isAuthenticated.next(false));
     else
       this.isAuthenticated.next(false);
-
   }
 }
