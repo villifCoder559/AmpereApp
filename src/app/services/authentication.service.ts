ac294773b2823bacc4ee7c47c405e81b2aa66000
VillifCoder559 <francesco.villi@gmail.com>
2022-03-03 18:35:23 +0100

    refactor code and new implementation

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,45 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 import { BehaviorSubject, from, Observable, Subject } from 'rxjs';
 import * as Keycloak from 'keycloak-ionic/keycloak';
-import { SharedDataService, UserData } from '../data/shared-data.service';
-import { Router } from '@angular/router';
-import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
   isAuthenticated: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(null);
+  requestTokenTimeout = null;
   public keycloak: Keycloak.KeycloakInstance;
-  constructor(private localNotifications: LocalNotifications, private shareData: SharedDataService, private router: Router) {
-    //this.loadToken();
-  }
-
-  // loadToken() {
-  //   const token = window.localStorage.getItem('TOKEN_KEY');
-  //   console.log('Token_get '+ token)
-  //   if (token != null) {
-  //     console.log('set token: ', token);
-  //     //this.shared_data.loadDataUser();
-  //     this.shared_data.setIs_logged(true);
-  //     this.isAuthenticated.next(true);
-  //   } else {
-  //     this.isAuthenticated.next(false);
-  //   }
-  // }
-
-  login(credentials: { email, password }): Observable<any> {
-    this.isAuthenticated.next(true)
-    return this.isAuthenticated
-    // return this.http.post(`https://reqres.in/api/login`, credentials).pipe(
-    //   map((data: any) => {
-    //     this.token = data.token;
-    //     window.localStorage.setItem('TOKEN_KEY', this.token);
-    //     this.shared_data.setIs_logged(true);
-    //     this.isAuthenticated.next(true);
-    //   })
-    // )
-  }
+  constructor() {}
+  
   loginSnap4City() {
     return new Promise<boolean>((resolve, reject) => {
       if (this.keycloak == null) {
@@ -57,22 +28,7 @@ export class AuthenticationService {
         .then((autentication) => {
           if (autentication) {
             this.keycloak.loadUserProfile().then((info: any) => {
-              this.keycloak.onTokenExpired = () => {
-                this.keycloak.updateToken(30).then((refreshed) => {
-                  if (refreshed)
-                    console.log('token refreshed correctly')
-                  else
-                    console.log('Token still valid')
-                }, err => {
-                  console.log(err)
-                  this.localNotifications.schedule({
-                    text: 'Token expired, login to continue using the app!'
-                  })
-                  this.logout();
-                })
-              }
-              this.shareData.user_data.id = info.username;
-              console.log(this.shareData.user_data.id)
+              //this.shareData.user_data.id = info.username;
               console.log('token ' + this.keycloak.token)
               console.log(info)
               resolve(true);
@@ -90,14 +46,13 @@ export class AuthenticationService {
         })
     })
   }
-
   logout() {
-    //this.bluetoothService.disableAll();
-    window.localStorage.removeItem('TOKEN_KEY');
-    this.shareData.user_data = new UserData();
-    if (this.keycloak != null)
-      this.keycloak.logout().then(() => this.isAuthenticated.next(false));
-    else
-      this.isAuthenticated.next(false);
+    return new Promise((resolve, reject) => {
+      //this.shareData.user_data = new UserData();
+      this.keycloak.logout().then(() => {
+        this.isAuthenticated.next(false)
+        resolve(true)
+      }, err => reject(err));
+    })
   }
 }
