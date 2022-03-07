32a2e6d1d208e3ed11378b284368f436e5c62468
VillifCoder559 <francesco.villi@gmail.com>
2022-03-07 18:38:54 +0100

    fix background_location permmission

--- src/app/pages/homepage/homepage.page.ts
+++ src/app/pages/homepage/homepage.page.ts
@@ -4,12 +4,11 @@ import { LocationAccuracy } from '@ionic-native/location-accuracy/ngx';
 import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
 import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 import { Platform } from '@ionic/angular';
-import { AlertEvent, DeviceType, QRNFCEvent, SharedDataService } from '../../data/shared-data.service'
+import { AlertEvent, DeviceType, Emergency_Contact, QRNFCEvent, SharedDataService, UserData } from '../../data/shared-data.service'
 import { NGSIv2QUERYService } from '../../data/ngsiv2-query.service'
 import { Snap4CityService } from '../../data/snap4-city.service'
 import { HttpClient } from '@angular/common/http';
 import { AndroidPermissions } from '@awesome-cordova-plugins/android-permissions/ngx';
-import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationEvents, BackgroundGeolocationResponse } from '@awesome-cordova-plugins/background-geolocation/ngx'
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.page.html',
@@ -18,74 +17,104 @@ import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocati
 
 export class HomepagePage implements OnInit {
   gps_enable = true;
-  constructor(private backgroundGeolocation: BackgroundGeolocation, private http: HttpClient, private s4c: Snap4CityService, private ngsi: NGSIv2QUERYService, private sharedData: SharedDataService, private platform: Platform, private localNotifications: LocalNotifications, private router: Router, private locationAccuracy: LocationAccuracy, private geolocation: Geolocation, private androidPermissions: AndroidPermissions) {
-
+  constructor(private http: HttpClient, private s4c: Snap4CityService, private ngsi: NGSIv2QUERYService, private sharedData: SharedDataService, private platform: Platform, private localNotifications: LocalNotifications, private router: Router, private locationAccuracy: LocationAccuracy, private geolocation: Geolocation, private androidPermissions: AndroidPermissions) {
+    this.enableAllPermission()
+  }
+  enableAllPermission() {
+    this.platform.ready().then(() => {
+      this.askPermission().then(() => {
+        this.sharedData.enableAllBackgroundMode();
+        console.log('ASK_PERMISSION')
+        this.localNotifications.hasPermission().then(result => {
+          if (!result.valueOf())
+            this.localNotifications.requestPermission()
+        }, (err) => console.log(err))
+      }, err => console.log(err))
+    })
   }
   ngOnInit() {
   }
   ngAfterViewInit() {
+    this.sharedData.dismissLoading().catch((err) => console.log(err))
     console.log('ngAfterViewInit')
-    this.platform.ready().then(() => {
-      this.sharedData.dismissLoading().then(() => {
-        this.askPermission().then(() => {
-          //this.checkPermission();
-          this.sharedData.enableAllBackgroundMode();
-          this.enableGPS();
-          console.log('ASK_PERMISSION')
-          this.localNotifications.hasPermission().then(result => {
-            if (!result.valueOf())
-              this.localNotifications.requestPermission()
-          }, (err) => console.log(err))
-        }, err => console.log(err));
-      }, (err) => console.log(err))
-    }), (err => console.log(err));
   }
   enableGPS() {
-    this.locationAccuracy.request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY).then(
-      () => {
-        this.gps_enable = true;
-      },
-      error => {
-        alert(JSON.stringify(error))
-        this.gps_enable = false;
-      }
-    );
+    return new Promise((resolve, reject) => {
+      this.locationAccuracy.request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY).then(
+        () => {
+          this.gps_enable = true;
+          resolve(true)
+        },
+        error => {
+          alert(JSON.stringify(error))
+          this.gps_enable = false;
+          reject(false)
+        }
+      );
+    })
   }
   locationAccPermission() {
-    this.locationAccuracy.canRequest().then((canRequest: boolean) => {
-      if (canRequest) {
-      } else {
-        this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION)
-          .then(
-            () => {
-              this.enableGPS();
-            },
-            error => {
-              alert(error)
-              this.gps_enable = false;
-            }
-          );
-      }
-    });
+    return new Promise((resolve, reject) => {
+      this.locationAccuracy.canRequest().then((canRequest: boolean) => {
+        if (canRequest) {
+          resolve(true)
+        } else {
+          this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION)
+            .then(
+              () => {
+                this.enableGPS().then(() => {
+                  resolve(true)
+                }, err => reject(err));
+              },
+              error => {
+                reject(error)
+                this.gps_enable = false;
+              }
+            );
+        }
+      });
+    })
   }
-  async checkPermission() {
+  checkPermission() {
     return new Promise((resolve, reject) => {
       this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION).then(
         result => {
           if (result.hasPermission) {
-            this.enableGPS();
+            this.enableGPS().then(() => {
+              console.log('hasPermission')
+              resolve(true)
+            });
           } else {
-            this.locationAccPermission();
+            console.log('requestPermission')
+            this.locationAccPermission().then(() => {
+              console.log('requestPermissionDone')
+              resolve(true)
+            });
           }
         },
-        error =>
+        error => {
+          console.log(error)
           reject(error)
+        }
       );
     })
   }
   showAlert() {
     //take bluetooth singal
     this.sharedData.showAlert('APP');
+  }
+  showLoading() {
+    this.sharedData.presentLoading('Test 1').then(() => {
+      setTimeout(() => {
+        this.sharedData.setTextLoading('Test setting new text')
+        setTimeout(() => {
+          this.sharedData.dismissLoading();
+        }, 2000)
+      }, 2000)
+    })
+  }
+  testSendStatus() {
+    this.ngsi.updateEntity({ 'status': this.sharedData.user_data.status }, DeviceType.PROFILE).catch((err) => console.log(err))
   }
   testQuery() {
   }
@@ -105,8 +134,20 @@ export class HomepagePage implements OnInit {
     //   console.log(result.address.value)
     // }, (err) => console.log(err))
   }
-  testWriteQuery() {
-
+  testEqualTo() {
+    var user1 = new UserData()
+    var user2 = new UserData()
+    user1.disabilities.visionImpaired = true;
+    user2.disabilities.visionImpaired = false;
+    // user1.paired_devices.push('test0')
+    // user1.paired_devices.push('test1')
+    // user2.paired_devices.push('test0')
+    // user2.paired_devices.push('test2')
+    // user1.emergency_contacts.push(new Emergency_Contact('name','surname','1234'))
+    // user1.emergency_contacts.push(new Emergency_Contact('name','surname','5678'))
+    // user2.emergency_contacts.push(new Emergency_Contact('name','surname','1234'))
+    // user2.emergency_contacts.push(new Emergency_Contact('name','surname','5678'))
+    console.log('REUSLT-> ' + user1.isEqualTo(user2))
   }
   openAlertPage() {
     let navigationExtras: NavigationExtras = {
@@ -130,7 +171,7 @@ export class HomepagePage implements OnInit {
     }, err => console.log(err))
   }
   SendEventQRNFC() {
-    var attr = this.ngsi.sendQRNFCEvent(new QRNFCEvent('', '', ''))
+    var attr = this.ngsi.sendQRNFCEvent(new QRNFCEvent('', '', '', -1, -1))
   }
   testDeviceID() {
     this.sharedData.showAlert('25a')
@@ -148,16 +189,28 @@ export class HomepagePage implements OnInit {
   /** From API 28 must ask about perimission ACCESS_BACKGROUND_LOCATION (android) */
   askPermission() {
     return new Promise((resolve, reject) => {
-      this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.ACCESS_BACKGROUND_LOCATION).then((response) => {
-        console.log(response)
-        resolve(true)
-      }, err => reject(err))
+      this.androidPermissions.checkPermission("android.permission.ACCESS_BACKGROUND_LOCATION").then((value) => {
+        if (!value.hasPermission) {
+          this.geolocation.getCurrentPosition().then((position) => {
+            alert('This app needs location access for all the time. Allow it so this app can work properly')
+            this.androidPermissions.requestPermissions([
+              "android.permission.ACCESS_BACKGROUND_LOCATION",
+              "android.permission.ACCESS_COARSE_LOCATION",
+              this.androidPermissions.PERMISSION.ACCESS_FINE_LOCATION,
+            ]).then((response) => {
+              console.log('ReusltResponse')
+              console.log(response)
+              resolve(true)
+            }, err => reject(err))
+          }, err => reject(err))
+        }
+      })
     })
   }
   checkAndroidPermission() {
     this.androidPermissions.checkPermission('ACCESS_BACKGROUND_LOCATION').then((value) => {
       if (!value.hasPermission)
-        this.androidPermissions.requestPermission('ACCESS_BACKGROUND_LOCATION');
+        this.askPermission();
       console.log(value)
     })
   }
@@ -170,35 +223,5 @@ export class HomepagePage implements OnInit {
     window.addEventListener("devicemotion", function (event) {
       console.log(event)
     }, true);
-  }
-  backgroundGeolocationMauron() {
-    setTimeout(() => {
-      const config: BackgroundGeolocationConfig = {
-        desiredAccuracy: 0,
-        stationaryRadius: 20,
-        distanceFilter: 50,
-        debug: true, //  enable this hear sounds for background-geolocation life-cycle.
-        stopOnTerminate: false, // enable this to clear background location settings when the app terminates
-        interval: 3000,
-        fastestInterval: 3000,
-        activitiesInterval: 3000,
-      };
-      this.backgroundGeolocation.configure(config)
-        .then(() => {
-          this.backgroundGeolocation.start();
-        });
-      // start recording location
-      setInterval(() => {
-        this.backgroundGeolocation.getCurrentLocation().then((position) => {
-          console.log(position)
-        })
-      }, 2000)
-      setTimeout(() => {
-        this.backgroundGeolocation.stop()
-      }, 20000)
-    }, 5000)
-
-    // If you wish to turn OFF background-tracking, call the #stop method.
-    //this.backgroundGeolocation.stop();
   }
 }
