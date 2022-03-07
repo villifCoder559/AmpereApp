0b39438a1ffb5af97b1c8a9e3b7cb79e20203190
VillifCoder559 <francesco.villi@gmail.com>
2022-03-07 18:37:15 +0100

    add get position when read code

--- src/app/data/send-auth.service.ts
+++ src/app/data/send-auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { NGSIv2QUERYService } from './ngsiv2-query.service';
-import { SharedDataService } from './shared-data.service';
+import { DeviceType, SharedDataService } from './shared-data.service';
 import { JwtHelperService } from '@auth0/angular-jwt'
 import { AuthenticationService } from '../services/authentication.service';
 import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { Platform } from '@ionic/angular';
+import BackgroundFetch from "cordova-plugin-background-fetch";
 
 const helper = new JwtHelperService();
 @Injectable({
@@ -13,26 +15,50 @@ export class SendAuthService {
   interval_active_user = null;
   requestTokenTimeout = null;
 
-  constructor(private localNotifications: LocalNotifications, private shared_data: SharedDataService, private ngsi: NGSIv2QUERYService, private authService: AuthenticationService) { }
+  constructor(private platform: Platform, private localNotifications: LocalNotifications, private shared_data: SharedDataService, private ngsi: NGSIv2QUERYService, private authService: AuthenticationService) {
+    this.platform.ready().then(this.onDeviceReady.bind(this));
+  }
+  async onDeviceReady() {
+    // Your BackgroundFetch event handler.
+    console.log('OnDeviceReady')
+    let onEvent = async (taskId) => {
+      console.log('[BackgroundFetch] event received: ', taskId);
+      this.checkAndRequestValidToken()
+      // Required: Signal completion of your task to native code
+      // If you fail to do this, the OS can terminate your app
+      // or assign battery-blame for consuming too much background-time
+      BackgroundFetch.finish(taskId);
+    };
 
+    // Timeout callback is executed when your Task has exceeded its allowed running-time.
+    // You must stop what you're doing immediately BackgroundFetch.finish(taskId)
+    let onTimeout = async (taskId) => {
+      console.log('[BackgroundFetch] TIMEOUT: ', taskId);
+      BackgroundFetch.finish(taskId);
+    };
+    // Configure the plugin.
+    let status = await BackgroundFetch.configure({ minimumFetchInterval: 15 }, onEvent, onTimeout);
+    console.log('[BackgroundFetch] configure, status: ', status);
+  }
   sendStatus(state: "expired" | "valid") {
     this.shared_data.user_data.dateObserved = new Date().toISOString();
     if (state == 'expired')
       this.shared_data.user_data.status = 'token_expired'
     else
       this.shared_data.user_data.status = 'active'
-    this.ngsi.sendUserProfile().catch((err) => console.log(err))
+    this.ngsi.updateEntity({ "status": { "value": this.shared_data.user_data.status } }, DeviceType.PROFILE).catch((err) => console.log(err))
   }
   startSendingValidStatus() {
     var status: "valid"
-    console.log(typeof(status))
+    console.log(typeof (status))
     let time = 1000 * 60 * 30;
     this.sendStatus(status);
     this.interval_active_user = setInterval(() => {
-      this.shared_data.user_data.status = typeof(status)
+      this.shared_data.user_data.status = typeof (status)
       this.ngsi.sendUserProfile().catch((err) => console.log(err))
     }, time)
   }
+
   // sendAccessAndCheckStatusToken() {
   //   this.sendStatusToken('valid')
   //   var expiration_date_token = helper.getTokenExpirationDate(this.authService.keycloak.token);
@@ -42,23 +68,26 @@ export class SendAuthService {
   checkAndRequestValidToken() {
     var now_date = new Date();
     var expiration_date_token = new Date(helper.getTokenExpirationDate(this.authService.keycloak.token))
-    this.requestTokenTimeout = setTimeout(() => {
-      this.authService.keycloak.updateToken(30).then((refreshed) => {
-        this.shared_data.accessToken = this.authService.keycloak.token;
-        if (refreshed)
-          console.log('Token updated successfully')
-        else
-          console.log('token still valid')
-        this.checkAndRequestValidToken();
-      }, err => {
-        console.log(err)
-        this.sendStatus('expired');
-        this.localNotifications.schedule({
-          text: 'Session is expired, login to contine using application! ',
-          launch: true
-        })
+    console.log('EXPIRATION_TOKEN')
+    console.log(expiration_date_token)
+    //this.requestTokenTimeout = setTimeout(() => {
+    this.authService.keycloak.updateToken(30).then((refreshed) => {
+      this.shared_data.accessToken = this.authService.keycloak.token;
+      if (refreshed)
+        console.log('Token updated successfully')
+      else
+        console.log('token still valid')
+      this.sendStatus('valid')
+      //this.checkAndRequestValidToken();
+    }, err => {
+      console.log(err)
+      this.sendStatus('expired');
+      this.localNotifications.schedule({
+        text: 'Session is expired, login to contine using application! ',
+        launch: true
       })
-    }, expiration_date_token.getTime() - now_date.getTime())
+    })
+    //}, expiration_date_token.getTime() - now_date.getTime())
   }
   clearAllInterval() {
     clearInterval(this.interval_active_user)
