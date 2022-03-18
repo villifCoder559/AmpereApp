44482b7b614cf355c09312953d0be4026ad01411
VillifCoder559 <francesco.villi@gmail.com>
2022-03-18 20:49:30 +0100

    add tour and tutorial

--- src/app/pages/faq/faq.page.ts
+++ src/app/pages/faq/faq.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SharedDataService } from 'src/app/data/shared-data.service';
 
 @Component({
   selector: 'app-faq',
@@ -7,10 +8,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./faq.page.scss'],
 })
 export class FaqPage implements OnInit {
-  collapsedHeight='90px';
-  constructor() { }
+  collapsedHeight = '90px';
+  constructor(private router: Router, private shared_data: SharedDataService) { }
 
   ngOnInit() {
   }
-  
+  startTour() {
+    this.router.navigateByUrl('profile/menu/homepage').finally(()=>{
+      setTimeout(()=>{
+        this.shared_data.startTour();
+      },500)
+    })
+  }
+  openTutorial() {
+    this.router.navigateByUrl('tutorial', { replaceUrl: true })
+  }
 }
