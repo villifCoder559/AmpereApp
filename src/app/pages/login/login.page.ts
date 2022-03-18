89a0d461354602f5ee8cc2bfee3d93448c4da043
VillifCoder559 <francesco.villi@gmail.com>
2022-03-18 18:22:07 +0100

    add tutorial and check registration

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,6 @@
 import { AuthenticationService } from './../../services/authentication.service';
 import { Component, OnInit } from '@angular/core';
-import {  FormGroup } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { DeviceType, SharedDataService, StorageNameType } from '../../data/shared-data.service'
@@ -8,6 +8,8 @@ import * as Keycloak from 'keycloak-ionic/keycloak';
 import { BluetoothService } from 'src/app/data/bluetooth.service';
 import { NGSIv2QUERYService } from 'src/app/data/ngsiv2-query.service';
 import { SendAuthService } from 'src/app/data/send-auth.service';
+import { Storage } from '@ionic/storage-angular';
+import { Network } from '@awesome-cordova-plugins/network/ngx'
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -23,10 +25,26 @@ export class LoginPage implements OnInit {
     private toastCtrl: ToastController,
     private bluetoothService: BluetoothService,
     private ngsi: NGSIv2QUERYService,
-    private sendAuth:SendAuthService) { }
+    private sendAuth: SendAuthService,
+    private storage: Storage,
+    private network:Network) {}
 
   ngOnInit() {
-
+    this.storage.create().then(()=>{
+      this.storage.get('tutorial_read').then((read)=>{
+        if (read != true)
+        this.router.navigateByUrl('/tutorial', { replaceUrl: true })
+      })
+    },err=>console.log(err))
+    this.network.onDisconnect().subscribe(()=>{
+      alert('This app needs internet connection, enable it!');
+    })
+  }
+  registration() {
+    this.router.navigateByUrl('/registration-s4c', { replaceUrl: true })
+  }
+  openTutorial() {
+    this.router.navigateByUrl('/tutorial', { replaceUrl: true })
   }
   loginTest() {
     this.authService.isAuthenticated.next(true);
@@ -54,7 +72,7 @@ export class LoginPage implements OnInit {
               Object.keys(this.sharedData.localStorage).forEach((element: StorageNameType) => {
                 this.sharedData.getNameDevices(element);
               })
-              this.router.navigateByUrl('/profile/menu/homepage', { replaceUrl: true }).then(()=>{
+              this.router.navigateByUrl('/profile/menu/homepage', { replaceUrl: true }).then(() => {
                 this.sharedData.dismissLoading();
               })
               console.log('END_ROUTER')
@@ -69,7 +87,7 @@ export class LoginPage implements OnInit {
         }
       }, async (err) => {
         console.log(err)
-        alert(err===undefined?'Check your internet connection':err)
+        alert(err === undefined ? 'Check your internet connection' : err)
         await this.sharedData.dismissLoading();
       });
     } catch (e) {
