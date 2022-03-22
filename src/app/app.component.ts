d601e8677b1c8b3ecc8ceb6e49bffc888b44b8b8
VillifCoder559 <francesco.villi@gmail.com>
2022-03-22 23:12:24 +0100

    add bilingual

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { Pipe, PipeTransform} from "@angular/core";
+import { Pipe, PipeTransform } from "@angular/core";
 import { DomSanitizer } from "@angular/platform-browser";
+import { Router } from '@angular/router';
 import { LottieSplashScreen } from '@awesome-cordova-plugins/lottie-splash-screen/ngx';
 import { Platform } from '@ionic/angular';
+import { LanguageService } from './data/language.service';
 
 @Pipe({ name: 'safe' })
 export class SafePipe implements PipeTransform {
@@ -17,7 +19,19 @@ export class SafePipe implements PipeTransform {
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {
+  constructor(private lottieSplashScreen:LottieSplashScreen,private plt: Platform, private language: LanguageService, private router: Router) {
+    this.initializeApp().then(()=>console.log('EndInit'))
   }
-
+  initializeApp() {
+    return new Promise((resolve) => {
+      this.plt.ready().then(() => {
+        this.language.setInitialAppLanguage().then(() => {
+         // this.router.navigateByUrl('/login').then(() => {
+            this.lottieSplashScreen.hide();
+            resolve(true)
+        //  })
+        });
+      })
+    })
+  }
 }
