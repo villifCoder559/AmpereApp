f6adec16faff85aed48a6fb19c1ac7a8154f4b33
VillifCoder559 <francesco.villi@gmail.com>
2022-04-03 22:00:12 +0200

    change background mode

--- src/app/pages/homepage/homepage.module.ts
+++ src/app/pages/homepage/homepage.module.ts
@@ -10,7 +10,7 @@ import { HomepagePage } from './homepage.page';
 import { MaterialModule } from '../../material-module'
 import { LocationAccuracy } from '@ionic-native/location-accuracy/ngx';
 import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
-import { BackgroundMode } from '@ionic-native/background-mode/ngx';
+//import { BackgroundMode } from '@ionic-native/background-mode/ngx';
 import { LocalNotifications } from '@awesome-cordova-plugins/local-notifications/ngx';
 import { AndroidPermissions } from '@awesome-cordova-plugins/android-permissions/ngx';
 import { BackgroundGeolocation} from '@awesome-cordova-plugins/background-geolocation/ngx'
