89d4bd31479c1f9df83d587930c8cb489df5c722
VillifCoder559 <francesco.villi@gmail.com>
2022-04-03 09:40:05 +0200

    show faq webpage

--- src/app/pages/faq/faq.page.ts
+++ src/app/pages/faq/faq.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { TourService } from 'ngx-ui-tour-md-menu';
+import { LanguageService } from 'src/app/data/language.service';
 import { SharedDataService } from 'src/app/data/shared-data.service';
 
 @Component({
@@ -8,19 +12,32 @@ import { SharedDataService } from 'src/app/data/shared-data.service';
   styleUrls: ['./faq.page.scss'],
 })
 export class FaqPage implements OnInit {
-  collapsedHeight = '90px';
-  constructor(private router: Router, private shared_data: SharedDataService) { }
+  urlSafe;
+  constructor(private translate:TranslateService,private tourService: TourService,private languageService: LanguageService, private router: Router, private shared_data: SharedDataService, public sanitizier: DomSanitizer) {
+    if (this.languageService.selected_language.value == 'it')
+      this.urlSafe = this.sanitizier.bypassSecurityTrustResourceUrl('https://www.snap4city.org/drupal/node/781');
+    else
+      this.urlSafe = this.sanitizier.bypassSecurityTrustResourceUrl('https://www.snap4city.org/drupal/node/782');
+  }
 
   ngOnInit() {
+    setTimeout(()=>{
+      this.tourService.initialize([{
+        anchorId:'button_tour',
+        title: this.translate.instant('FAQ.tour.button_tour.title'),
+        content: this.translate.instant('FAQ.tour.button_tour.content'),
+        nextBtnTitle: this.translate.instant('TOUR.button.end'),
+        prevBtnTitle:'',
+        enableBackdrop: true
+      }])
+      this.tourService.start();
+    },1200)
   }
   startTour() {
-    this.router.navigateByUrl('profile/menu/homepage').finally(()=>{
-      setTimeout(()=>{
+    this.router.navigateByUrl('profile/menu/homepage').finally(() => {
+      setTimeout(() => {
         this.shared_data.startTour();
-      },500)
+      }, 650)
     })
-  }
-  openTutorial() {
-    this.router.navigateByUrl('tutorial', { replaceUrl: true })
   }
 }
