18dfc664dcacee6bcedf9b514e54232781a3d8bb
VillifCoder559 <francesco.villi@gmail.com>
2022-04-10 22:56:40 +0200

    first commit

---
+++ src/app/data/storage.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+import { Storage } from '@ionic/storage-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+  private _storage: Storage | null = null;
+
+  constructor(private storage: Storage) {
+    this.init();
+  }
+
+  async init() {
+    // If using, define drivers here: await this.storage.defineDriver(/*...*/);
+    const storage = await this.storage.create();
+    console.log(this.storage.length())
+    this._storage = storage;
+  }
+
+  // Create and expose methods that users of this service can
+  // call, for example:
+  public set(key: string, value: any) {
+    this._storage?.set(key, value);
+  }
+  public get(key: string) {
+    return new Promise((resolve, reject) => {
+      this._storage?.get(key).then((result) => {
+        resolve(result)
+      }, err => reject(err));
+    })
+  }
+}
